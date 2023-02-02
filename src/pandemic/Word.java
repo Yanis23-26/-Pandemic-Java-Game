569a98d6aec5ed75230fb28001f1dd88851537bf
SEGHIR <anes.seghir.etu@univ-lille.fr>
2023-02-02 10:10:24 +0100

    class word

---
+++ src/pandemic/Word.java
@@ -0,0 +1,33 @@
+package pandemic;
+
+import java.util.List;
+
+public class Word { // this class contains all the sectors 
+	private List<sectors> sectors; // the list of sectors in the map
+	    public Word(){
+        this.sectors=new Arraylist<>();
+    }
+	/** 
+	fonction getSectors return the list of sectors
+	@return  list of sectors
+	
+	*/
+	
+	public List<sector> getSectors(){
+        return this.sectors;
+    }
+	/** 
+	fonction addSector add the sector to the  list of sectors
+	@param sector
+	*/
+	public void addSector(Sector sector){
+        this.sectors.add(sector);
+    }
+	
+
+}
+
+
+
+
+
