1d3dfcdd13cb7f2b045e8e28ecc0ff5acd63a8e2
Slimani <rayanemalik.slimani.etu@univ-lille.fr>
2023-02-02 10:20:12 +0100

    the ResearchStation class

--- src/pandemic/ResearchStation.java
+++ src/pandemic/ResearchStation.java
@@ -6,19 +6,37 @@ public class ResearchStation {
 	
 	
 	
+	/**
+	 * ResearchStation constructor
+	 * @param name String, the research station name
+	 * @param city City, the city where the research station is located 
+	 */
 	public ResearchStation(String name, City city ) {
 		this.city=city;
 		this.name=name;
 	}
 	
+	/**
+	 * returns the name of the research station
+	 * @return the name
+	 */
 	public String getName() {
 		return this.name;
 	}
+	
+	/**
+	 * returns the city where research station is located
+	 * @return the city
+	 */
 	public City getCity() {
 		return this.city;
 		
 	}
 	
+	/**
+	 * changes the city location of the research station
+	 * @param city City, the new city of research station
+	 */
 	public void ChangeCity(City city) {
 		this.city=city ;
 		
