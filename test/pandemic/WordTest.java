2875523b6837ccbbaace2178d843c5b77e75a7e1
Slimani <rayanemalik.slimani.etu@univ-lille.fr>
2023-02-06 16:27:16 +0100

    modéfication test

--- test/pandemic/WordTest.java
+++ test/pandemic/WordTest.java
@@ -1,15 +1,44 @@
 package pandemic;
 
 import static org.junit.Assert.*;
-
+import org.junit.Before;
 import org.junit.Test;
+import pandemic.*;
 
-public class WordTest {
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
-		//rayane
-	}
 
+
+public class WorldTest {
+    private List<Sector> sectors;
+    private Sector sector;
+    private Disease disease; 
+    private int id;
+
+    @test
+    public void addSectorTest(){
+        this.sectors=new ArrayList<>();
+        this.sector=new Sector("sector1");
+        this.disease= new Disease("gripe",this.sector,0);
+        sectors.addSector(sector);
+        assertEquals(sectors.length(),1);
+        assertEquals(sectors[0],sector);
+
+
+    }
+
+
+
+   
+
+
+
+
+   // ---Pour permettre l'execution des tests ----------------------
+   public static junit.framework.Test suite() {
+      return new junit.framework.JUnit4TestAdapter(pandemic.WorldTest.class);
+   }
 }
+
+
+
+ 
