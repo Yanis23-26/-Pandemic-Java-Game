a68ac4a46ea7708b5c2cc5adeecad7b2169810ab
Slimani <rayanemalik.slimani.etu@univ-lille.fr>
2023-02-07 09:22:47 +0100

    class Disease

--- src/pandemic/Disease.java
+++ src/pandemic/Disease.java
@@ -1,22 +1,31 @@
 package pandemic;
 
 public class Disease {
-	private String name;
-	private int id;
-	
-	
-	
-	public Disease(String name, int id) {
+	private String name;//name of disease
+	private int id;//id of Disease
+
+
+	/**
+	 * Disease constructor
+	 * @param name String,the name of disease
+	 * @param id int, id of disease
+	 */
+	public Disease(String name, int id){
 		this.name=name;
 		this.id=id;
 	}
-	
+	/**
+	 * @return the name of disease
+	 */
 	public String getName() {
 		return this.name;
 	}
-	public int getSector(){
+	/**
+	 * @return the id of disease
+	 */
+	public int getId(){
 		return this.id;
 	}
 
-	
+
 }
