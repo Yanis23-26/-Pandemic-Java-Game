93415106c72947c0f5f4e57db15ec6d37ca85225
Diaf <manil.diaf.etu>
2023-04-04 02:13:13 +0200

    displayIntro & displayPlayersInfo

--- src/pandemic/Game.java
+++ src/pandemic/Game.java
@@ -6,7 +6,7 @@ import pandemic.Cards.*;
 import pandemic.Roles.*;
 import java.util.*;
 import java.util.Collections;
-
+import java.util.Scanner;
 
 
 /**
@@ -37,7 +37,6 @@ public class Game {
 		this.drawEpidemic = new Stack<EpidemicCard>();
 		this.players = new ArrayList<Player>();
 		initializeDraw();
-		initializationListPlayer();
 		
 	}
 	
@@ -86,19 +85,6 @@ public class Game {
 	}
 
 	
-	/*
-	 * Initialize the list of Players
-	 */
-	public void initializationListPlayer() {
-		// La ville sur laquelle les 4 joueurs vont se trouver :
-		City city = world.getSectors().get(0).getCities().get(0);
-		this.players.add(new GlobePlotter("manil",city));
-		this.players.add(new Doctor("rayane",city));
-		this.players.add(new Scientist("yanis",city));
-		this.players.add(new Expert("anes",city));
-	}
-	
-	
 	
 	/**
 	 * a player take a card 
@@ -128,6 +114,7 @@ public class Game {
 	}
 	
 	// FONCTIONS POUR MELENGER LES DECKS
+	
 	public  void shufflePlayerCards(Stack<PlayerCard> playerCards) {
 	    Collections.shuffle(playerCards);
 	}
@@ -140,26 +127,96 @@ public class Game {
 	
 	
 	
-
+	//Les fonctions d affichages :
 	
-	/*
-	 * Main game loop
-	 */
-	public void play() {
-		/*
-		 * tire 2 cartes de la pile des cartes infection et réalise l’infection associée
-		 
-		- pour chaque joueur tire 2 cartes 
-		 -de la pile des cartes joueurs et les ajoute à la main du joueur, le cas échéant déclenche une épidémie
-		*/
-		DrawAInfectionCard(this.players.get(0));
-		DrawAInfectionCard(this.players.get(0));
-		
-		for(Player p : this.players) {
-			DrawAPlayerCard(p);
-			DrawAPlayerCard(p);
+	//display intro
+		public void displayIntro() {
+		System.out.println("##########################################");
+	    System.out.println("#             P A N D E M I C             #");
+	    System.out.println("#                 G A M E                #");
+	    System.out.println("##########################################\n");
+	    System.out.println("Welcome to Pandemic Game! In this game, you will work as a team to stop the spread of deadly diseases across the globe.\n");
+	    System.out.println("Each player will take on a specific role with unique abilities, such as a scientist who can discover a cure faster or a medic who can treat multiple infected people at once.\n");
+	    System.out.println("You must work together to collect the necessary cards and research stations, discover cures, and treat and eradicate the diseases before they become too widespread.\n");
+	    System.out.println("Are you ready to save the world? Let's begin!\n");
+	    System.out.println("##########################################\n");
+	    Scanner scanner = new Scanner(System.in);
+	    System.out.println("Press any key to continue...");
+	    scanner.nextLine();
+	    
 		}
 		
 		
+		
+		
+		/// Demander le nombre de joueurs
+		public void displayPlayersInfo() {
+		// on choisis une ville au hasard ou nos joueurs vont apparaitre
+		City city = world.getSectors().get((int)(Math.random() * 5)).getCities().get((int)(Math.random() * 12));	
+		int numPlayers = 0;
+		boolean validInput = false;
+		Scanner scanner = null;
+		while (!validInput) {
+		System.out.print("How many players will be playing? (2-4) ");
+		try {
+		numPlayers = scanner.nextInt();
+		if (numPlayers >= 2 && numPlayers <= 4) {
+		validInput = true;
+		} else {
+		System.out.println("Please enter a number between 2 and 4.");
+		}
+		} catch (InputMismatchException e) {
+		System.out.println("Please enter a valid number.");
+		scanner.next();
+		}
+		}
+
+		// Définir les rôles disponibles
+		List<String> availableRoles = new ArrayList<>(Arrays.asList("doctor", "expert", "globetrotter", "scientist"));
+
+		// Demander le nom et le rôle de chaque joueur
+		scanner.nextLine();
+		for (int i = 0; i < numPlayers; i++) {
+		System.out.printf("\nPlayer %d, enter your name: ", i + 1);
+		String name = scanner.nextLine();
+
+
+		boolean validRole = false;
+		while (!validRole) {
+		    System.out.printf("Choose your role, %s. Available roles: %s ", name, availableRoles.toString());
+		    String role = scanner.nextLine().toLowerCase();
+		    if (availableRoles.contains(role)) {
+		        switch (role) {
+		            case "doctor":
+		            	this.players.add(new Doctor(name,city));
+		                break;
+		            case "expert":
+		            	this.players.add(new Expert(name,city));
+		                break;
+		            case "globetrotter":
+		            	this.players.add(new GlobePlotter(name,city));
+		                break;
+		            case "scientist":
+		            	this.players.add(new Scientist(name,city));
+		                break;
+		        }
+		        availableRoles.remove(role);
+		        validRole = true;
+		    } else {
+		        System.out.println("Invalid role. Please choose one of the available roles.");
+		    }
+		}
+
+		}
+		}
+	
+	
+	
+	
+	
+
+
+
+	public void play() {
 	}
 }
