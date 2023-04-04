a8654090b15c6b30a41488da34bb2802cb989173
SEGHIR <anes.seghir.etu@univ-lille.fr>
2023-04-05 00:21:05 +0200

    IntermediateCardTest

--- test/pandemic/Cards/IntermediateCardTest.java
+++ test/pandemic/Cards/IntermediateCardTest.java
@@ -33,6 +33,36 @@ public class IntermediateCardTest{
         IntermediateCard card2=new IntermediateCard(paris,ebola);
         assertEquals(ebola, card2.getDisease());
 	}
+   /**
+   ce teste pour tester le fonctionement de la méthode equals
+    */
+   @Test
+   public void equalsTest(){
+    Disease ebola = new Disease("Ebola",2);
+    Sector europe= new Sector("EUROPE",ebola,2);
+    city Lille=new city("Lille",europe);
+
+
+    Disease covid = new Disease("covid19",2);
+    Sector europe= new Sector("EUROPE",covid,2);
+    city paris=new city("paris",europe);
+
+    IntermediateCard card1=new IntermediateCard(Lille,ebola);
+    IntermediateCard card2=new IntermediateCard(Lille,ebola);
+
+    assertTrue(card1.equals(card2));
+
+
+    IntermediateCard card3=new IntermediateCard(Lille,ebola);
+    IntermediateCard card4=new IntermediateCard(paris,covid);
+
+    assertFalse(card3.equals(card4));
+
+    
+
+
+
+   }  
 
 
 
