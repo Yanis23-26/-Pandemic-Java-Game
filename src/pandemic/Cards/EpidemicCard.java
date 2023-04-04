93a4048cc79de634a6fc8ce7fde52ff778e9b7df
SEGHIR <anes.seghir.etu@univ-lille.fr>
2023-04-04 20:36:05 +0200

    classe epedemiccard modification

--- src/pandemic/Cards/EpidemicCard.java
+++ src/pandemic/Cards/EpidemicCard.java
@@ -1,36 +1,40 @@
 package pandemic.Cards;
+import pandemic.Board.*;
+
+// cette classe represente la carte de l'epidemie
+public class EpidemicCard implements Card{
+	private Stack<card> cards; //une pile qui prends des objets de type card
 
 
-
-public class EpidemicCard implements Card, PileCardPlayer{
-	
-	// "Sur la carte epidémie, il n ya pas d'information particulière" sujet du projet
-	
-
-	
-	public EpidemicCard() {
-		super();
+	public EpidemicCard(){
+		cards=new Stack<>();
 	}
-	
-	
-	
+
+
 	/**
-	 * Permet de gérer ce qui se passe lorsqu'un joueur tire une carte epidémie
+	cette fonction ajoute des cards dans la pile des cards
+
+	*/
+     public void addCard(InfectionCard card){
+		cards.push(card)	 
+	} 
+	
+	
+	
+	
+  
+	/**
+	 * Allows you to manage what happens when the card is drawn
 	 */
 	public void comportement() {
-		/* Comportement triple : 
- 		1- Augmente le taux globale d'infection 
- 		2- Declenche une phase d'infection quelque soit la valeur du taux
- 		3- Mélange des cartes de la defausse
-		*/
-		
-		
-		
+			// on tire une carte infection 
+			// on augmente le taux global d'infection 
+			// on melange les cartes d'infections
+			this.city.addInfection(disease);
+			this.city.infectNeighbors(disease);
+			
+			
 	}
-}
-   /*
-   this.city.addInfection(disease);
-   this.city.infectNeighbors(disease);
-   InfectionDeck.getInstance().shuffle();
-   */
 
+
+} 
