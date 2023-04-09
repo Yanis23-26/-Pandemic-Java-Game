e3823d435c23c5d78d23054ca6e590ee66344d68
SEGHIR <anes.seghir.etu@univ-lille.fr>
2023-04-09 17:47:21 +0200

    suite à une modification de la classe epedimiccard correction de la classe epedimiccardtest

--- test/pandemic/Cards/EpidemicCardTest.java
+++ test/pandemic/Cards/EpidemicCardTest.java
@@ -7,81 +7,59 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class EpidemicCardTest {
+   	   private Game game;
+	   private String path;
+	   @Before        
+	   public void init() throws FileNotFoundException {
+		  this.path="./src/pandemic/carte2.json";
+	      this.game= new Game(path);
+	   }
+
+      @Test
+      public void comportement(){
+        Disease covid19=new Disease("covid19",2);		
+		Sector europe= new Sector("EUROPE",covid19,2);
+        City Lille=new City("LILLE",europe);
+        Player anes=new Doctor("ANES",Lille,game);
+
+        Disease ebola=new Disease("ebola",2);		
+		Sector afrique= new Sector("AFRIQUE",ebola,2);
+        City Tizi=new City("Tizi",afrique);
+        Player rayane=new Scientist("RAYANE",Tizi,game);
+
+        Disease HepatiteE=new Disease("HepatiteE",2);		
+		Sector europe2= new Sector("EUROPE2",HepatiteE,2);
+        City Lyon=new City("Lyon",europe2);
+        Player manil=new Expert("MANIL",Lyon,game);
+
+        Disease grippe=new Disease("grippe",2);		
+		Sector amerique= new Sector("AMERIQUE",grippe,2);
+        City california=new City("california",amerique);
+        Player yanis=new GlobePlotter("YANIS",california,game);
+
+        EpidemicCard epidemiccard=new EpidemicCard();
+
+        InfectionCard card1=new InfectionCard(Lille,covid19);
+        InfectionCard card2=new InfectionCard(Tizi,ebola);
+        InfectionCard card3=new InfectionCard(Lyon,HepatiteE);
+        InfectionCard card4=new InfectionCard(california,grippe);
+
+        Stack<InfectionCard> infectioncards=game.getDrawInfection();
+        infectioncards.push(card1);
+        infectioncards.push(card2);
+        infectioncards.push(card3);
+        infectioncards.push(card4);
+
+        epidemiccard.comportement(anes);
+
+        assertEquals(infectioncards,infectioncards.contains(card4));
+        assertEquals(3,california.getInfectionRate(grippe));
+
+
+
+      }  
+  
    
-   /**
-   ce test est effectué pour tester la méthode addCard()
-   */
-   @Test
-   public void  addCardTest(){
-    EpidemicCard epidemicCard=new EpidemicCard();
-    Disease ebola=new Disease("Ebola",3);		
-	Sector europe= new Sector("EUROPE",ebola,3);
-    City Lyon=new City("Lyon",europe);
-    InfectionCard card1=new InfectionCard(Lyon,ebola);
-    epidemicCard.addCard(card1);
-
-    assertTrue(epidemicCard.cards.contains(card1));
-    
-   } 
-   /**
-   ce test est effectué pour vérifier le comportement de la méthode DrawnCard()
-   */
-   @Test 
-   public void drawnCardTest(){
-    EpidemicCard epidemicCard=new EpidemicCard();
-    Disease ebola=new Disease("Ebola",3);		
-	Sector europe= new Sector("EUROPE",ebola,3);
-    City Lyon=new City("Lyon",europe);
-    InfectionCard card1=new InfectionCard(Lyon,ebola);
-    epidemicCard.addCard(card1);
-
-    epidemicCard.drawCard();
-
-    assertFalse(epidemicCard.cards.contains(card1));
-
-   } 
-    /**
-    ce test est effectué pour vérifier le fonctionement de la méthode comportement
-    */
-   @Test
-   public void ComportementTest(){
-       EpidemicCard epidemicCard=new EpidemicCard();
-       //card1
-       Disease ebola1=new Disease("Ebola",1);		
-	   Sector europe1= new Sector("EUROPE",ebola1,1);
-       City Lyon=new City("Lyon",europe1);
-       InfectionCard card1=new InfectionCard(Lyon,ebola1);
-
-       //card2
-       Disease covid2=new Disease("covid19",1);		
-	   Sector europe2= new Sector("EUROPE",covid2,1);
-       City nice=new City("nice",europe2);
-       InfectionCard card2=new InfectionCard(nice,covid2);
-
-       //card3
-       Disease covid3=new Disease("covid19",1);		
-	   Sector europe3= new Sector("EUROPE",covid3,1);
-       City lille=new City("lille",europe3);
-       InfectionCard card3=new InfectionCard(lille,covid3);
-
-       //card4
-       Disease ebola4=new Disease("Ebola",1);		
-	   Sector europe4= new Sector("EUROPE",ebola4,1);
-       City paris=new City("paris",europe4);
-       InfectionCard card4=new InfectionCard(paris,ebola4);
-       
-
-       epidemicCard.addCard(card1);
-       epidemicCard.addCard(card2);
-       epidemicCard.addCard(card3);
-       epidemicCard.addCard(card4);
-
-       epidemicCard.comportement();
-       assertFalse(epidemicCard.cards.contains(card4));
-       assertEquals(ebola.getId(),2);
-       assertEquals(epidemicCard.cards.get(0),card3);
-
-   }   
 	    
 
 }
